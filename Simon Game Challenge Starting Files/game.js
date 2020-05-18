9e950f7e3b9e831ebaf586a23f369cf71145f0d4
Katelin Cherry <kdcherry55@gmail.com>
2020-05-18 09:08:51 -0500

    simon game

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,77 @@
+
+var buttonColours=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+var level=0;
+var started = false;
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length);
+});
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level "+level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  animatePress(randomChosenColour);
+
+}
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel-1]==userClickedPattern[currentLevel-1]){
+    console.log("success");
+    if (userClickedPattern.length == gamePattern.length){
+      setTimeout(function(){
+      nextSequence();
+    },1000);
+      userClickedPattern=[];
+    }
+  }
+  else{
+    console.log("wrong")
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+
+}
+
+function startOver(){
+  userClickedPattern=[];
+  gamePattern=[];
+  started=false;
+  level=[];
+  $("#level-title").text("Game Over. Press any key to restart.");
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+};
